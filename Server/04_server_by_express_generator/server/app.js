ac477d0551d3f0ba0d568488450b548d9b8c8f4c
Daehee Yoon <allendy11@gmail.com>
2022-03-31 20:49:06 +0900

    server/basic server

---
+++ Server/04_server_by_express_generator/server/app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+module.exports = app;
+
+//? express generator 를 이용하여 서버의 기본형태를 만드는게 가능
