691e575ec42c5a7aad7837632fec96d82b1832b2
Daehee Yoon <allendy11@gmail.com>
2022-04-20 23:17:05 +0900

    vuejs/19_vue-slot

---
+++ Vuejs/19_v-slot/client/src/components/ChildView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>This is {{ name }}</h1>
+    <slot></slot>
+    <br />
+    <div>header</div>
+    <slot name="header"></slot>
+    <br />
+    <div>body</div>
+    <slot name="body"></slot>
+    <br />
+    <div>footer</div>
+    <slot name="footer" :username="name"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "kimcoding",
+    };
+  },
+};
+</script>
+
+<style></style>
