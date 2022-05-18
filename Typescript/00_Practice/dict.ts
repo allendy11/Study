6a23f9f09321823a9180eb50b0eea056db2645ab
Daehee Yoon <allendy11@gmail.com>
2022-05-18 13:38:29 +0900

    typescript/practice/dict/add update, remove

--- Typescript/00_Practice/dict.ts
+++ Typescript/00_Practice/dict.ts
@@ -14,6 +14,12 @@ class Dict {
   def(term: string) {
     return this.words[term];
   }
+  update(term: string, def: string) {
+    this.words[term] = def;
+  }
+  remove(term: string) {
+    delete this.words[term];
+  }
 }
 class Word {
   constructor(public readonly term: string, public readonly def: string) {}
@@ -26,4 +32,7 @@ const money = new Word("money", "돈");
 dict.add(cap);
 dict.add(money);
 
+dict.update("money", "악");
+dict.remove("cap");
 console.log(dict.def("money"));
+console.log(dict.def("cap"));
