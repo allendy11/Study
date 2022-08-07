f956a79b2eb66f86972ca4a3e9638adba081c503
allendy11 <allendy11@gmail.com>
2022-08-08 08:28:08 +0900

    [java] Tutorial_06

--- Java/Tutorial_06/Main.java
+++ Java/Tutorial_06/Main.java
@@ -1,17 +1,30 @@
 package Java.Tutorial_06;
 
 public class Main {
+  final static int N = 30;
+
   public static void main(String[] args) {
+    // while
     int i = 1, sum = 0;
     while (i <= 1000) {
       sum += i++;
     }
     System.out.println(sum);
 
+    // for
     sum = 0;
     for (int n = 0; n < 10; n++) {
       sum += n;
     }
     System.out.println(sum);
+
+    // for 2
+    for (int j = N; j > 0; j--) {
+      for (int k = j; k > 0; k--) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
   }
+
 }
