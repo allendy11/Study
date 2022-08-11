bc7cfe7831fd97ee4317e28b60615df230070c6d
allendy11 <allendy11@gmail.com>
2022-08-11 10:09:06 +0900

    [Java] Tutorial 17-18

---
+++ Java/Tutorial/Tutorial_18/Main.java
@@ -0,0 +1,7 @@
+package Java.Tutorial.Tutorial_18;
+
+public class Main {
+  public static void main(String[] args) {
+    Student student = new Student("a", 20, 175, 70, "20221000", 4, 4.5);
+  }
+}
