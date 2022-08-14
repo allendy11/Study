a1ba6c43a2e60840c3722d497123f4f1d43bd52d
Daehee Yoon <allendy11@gmail.com>
2022-08-14 23:02:25 +0900

    [Java] practice JOptionPane + practice Lucene

--- Java/JOptionPane/Main.java
+++ Java/JOptionPane/Main.java
@@ -3,6 +3,7 @@ package Java.JOptionPane;
 import javax.swing.JOptionPane;
 
 public class Main {
+  
   public static void main(String[] args) {
     String name = JOptionPane.showInputDialog("Enter your name");
     JOptionPane.showMessageDialog(null, "Hello " + "name");
