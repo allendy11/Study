19b8b4612d4799c63c65c6cec50022c77fca85bc
Daehee Yoon <allendy11@gmail.com>
2022-08-16 11:43:09 +0900

    [Java] Lucene: practice04

---
+++ Java/Lucene/Practice04/src/Adder.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+
+public class Adder {
+
+	public static void add(Directory index, String newTaxDetail) throws IOException {
+		StandardAnalyzer analyzer = new StandardAnalyzer();
+		IndexWriterConfig config = new IndexWriterConfig(analyzer);
+		IndexWriter writer = new IndexWriter(index, config);
+		Document doc = new Document();
+		
+		LastDoc lastDoc = new LastDoc(index);
+		String newTaxId = Integer.toString(Integer.parseInt(lastDoc.taxId) +1);
+		
+		doc.add(new StringField("TaxID", newTaxId, Store.YES));
+		doc.add(new TextField("TaxDetail", newTaxDetail, Store.YES));
+		
+		writer.addDocument(doc);
+		writer.commit();
+		writer.close();
+		
+		
+		LastDoc addedDoc = new LastDoc(index);
+		System.out.println("AddedDocId : " + addedDoc.docId);
+		System.out.println("AddedTaxId : " + addedDoc.taxId);
+		System.out.println("AddedTaxDetail : " + addedDoc.taxDetail);
+	}
+
+}
---
+++ Java/Lucene/Practice04/src/Indexer.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+//import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+
+public class Indexer {
+
+	public static void parse(Directory index, String data_path, String output_path) throws IOException {
+		StandardAnalyzer analyzer = new StandardAnalyzer();
+		IndexWriterConfig config = new IndexWriterConfig(analyzer);
+		IndexWriter writer = new IndexWriter(index, config);
+		
+		// string -> token
+		BufferedReader reader = new BufferedReader(new FileReader(data_path));
+		String str;
+		while((str = reader.readLine()) != null ) {
+			String[] array = str.split("\\|");
+			ArrayList<String> list = new ArrayList<>(); 
+			for(int i=0 ; i < array.length ; i++) {
+				list.add(array[i].trim());
+			}
+			String taxId = list.get(0);
+			String taxDetail = list.get(1);
+			String taxDetail_2 = list.get(2);
+			if(taxDetail_2.length() > 0) {
+				taxDetail += " " + taxDetail_2;
+			}
+			addDoc(writer, taxId, taxDetail);
+		}
+		reader.close();
+		writer.close();
+	}
+
+	private static void addDoc(IndexWriter writer, String taxId, String taxDetail) throws IOException {
+		Document doc = new Document();
+		doc.add(new StringField("TaxID", taxId, Store.YES));
+		doc.add(new TextField("TaxDetail", taxDetail, Store.YES));
+		writer.addDocument(doc);
+	}
+	
+}
