578dadbbc46005b761c9b6931a012ffe1707f67d
allendy11 <allendy11@gmail.com>
2022-08-16 18:00:02 +0900

    [Java] Tutorial 20-24 + add subscription

---
+++ Java/Tutorial/Tutorial_22/Dog.java
@@ -0,0 +1,7 @@
+public interface Dog {
+  abstract void crying();
+
+  // public void show() { 에러 발생 : 메소드 선언만 가능할뿐 정의는 불가능하다. (추상클래스라면 가능)
+  // System.out.println("hi");
+  // }
+}
---
+++ Java/Tutorial/Tutorial_23/Banana.java
@@ -0,0 +1,8 @@
+import Java.Tutorial.Tutorial_20.Fruit;
+
+public class Banana extends Fruit {
+  public Banana() {
+    name = "Banana";
+    price = 12000;
+  }
+}
