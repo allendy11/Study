41ca5cfe87876d15b50156ae5f38c0be54ddb87e
Daehee Yoon <allendy11@gmail.com>
2022-08-21 17:56:53 +0900

    arrange

--- Database/Sequelize/practice/server/routes/comment.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getComments, getUserComments, postComment } = require("../controllers");
-router.get("/", getComments);
-router.post("/", postComment);
-
-module.exports = router;
--- Database/Sequelize/practice/server/routes/users.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const { addUser, getUserComments } = require("../controllers");
-/* GET users listing. */
-router.post("/", addUser);
-router.get("/:id", getUserComments);
-module.exports = router;
